2fee252e2d1cc5b3a0ad53276851b97d70b34bb2
unknown <freemindedjatin@gmail.com>
2023-11-30 16:36:21 +0530

    okk

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
-const express =require("express");
+// const express =require("express");
+// const cors = require('cors');
+// require('dotenv').config();
+// const app=express();
+// const {generate} = require('./router/generate'); 
+// const {send} = require('./router/send'); 
+
+// app.use(express.json());
+// app.use(cors());
+
+
+// app.use('/generate', generate);
+// app.use('/send',send)
+
+
+// let port=process.env.PORT
+
+// app.listen(port,()=>{
+//     console.log("Server Running")
+// })
+const cluster = require('cluster');
+const os = require('os');
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const app=express();
-const {generate} = require('./router/generate'); 
-const {send} = require('./router/send'); 
+
+const app = express();
+const { generate } = require('./router/generate');
+const { send } = require('./router/send');
 
 app.use(express.json());
 app.use(cors());
 
-
 app.use('/generate', generate);
-app.use('/send',send)
+app.use('/send', send);
 
+// Cluster logic
+if (cluster.isMaster) {
+  // Create worker processes
+  const numCPUs = os.cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-let port=process.env.PORT
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    // Optionally, restart the worker
+    cluster.fork();
+  });
+} else {
+  // Worker process logic
+  const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
-    console.log("Server Running")
-})
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} active `);
+  });
+}
